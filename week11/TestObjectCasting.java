f16240cb1bd0f7bb6a0f07b812f2d5d88dbb9910
ballcomz <ballcomz@gmail.com>
2021-02-17 02:17:05 +0700

    11

---
+++ week11/TestObjectCasting.java
@@ -0,0 +1,26 @@
+package week11;
+
+class Fruit{
+
+}
+class Apple extends Fruit{
+
+}
+class Orange extends Fruit{
+
+}
+class Banana{
+
+}
+
+
+public class TestObjectCasting {
+    Object a = new Apple();
+    Object b = new Apple();
+
+    Object c = b;
+    
+
+    
+    
+}
